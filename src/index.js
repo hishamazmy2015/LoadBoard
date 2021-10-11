95498ce77f7d55968bf769a6ee34b0bfdb1710de
hesham <hishamazmy2015@gmail.com>
2021-10-11 22:26:13 +0400

    changes to fix issues

--- src/index.js
+++ src/index.js
@@ -3,10 +3,9 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
 import { BrowserRouter } from "react-router-dom";
-import './bootstrap.min.css';
+import "./bootstrap.min.css";
 
 ReactDOM.render(
-
   <BrowserRouter>
     <App />{" "}
   </BrowserRouter>,
